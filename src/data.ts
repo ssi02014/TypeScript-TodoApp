75add471cd9581f35fb52fdb680de450db8473a4
JeonMinJae <ssi02014@naver.com>
2021-06-15 02:33:51 +0900

    fix(minjae/todoCollection): data 파일 분리 #4

---
+++ src/data.ts
@@ -0,0 +1,7 @@
+//타입 추론으로 타입 설정 따로 안함
+export const data = [
+  { id: 1, task: "장보기", complete: true },
+  { id: 2, task: "TS 학습하기", complete: false },
+  { id: 3, task: "리액트 학습하기", complete: true },
+  { id: 4, task: "취업하기", complete: false },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 import TodoItem from './TodoItem';
-
-//타입 추론으로 타입 설정 따로 안함
-const data = [
-  { id: 1, task: "장보기", complete: true },
-  { id: 2, task: "TS 학습하기", complete: false },
-  { id: 3, task: "리액트 학습하기", complete: true },
-  { id: 4, task: "취업하기", complete: false },
-];
+import {data} from './data';
 
 for (let i = 0; i < data.length; i++) {
   // let todoItem:TodoItem = new TodoItem(...); TodoItem 타입을 타입 추론 생략 가능
