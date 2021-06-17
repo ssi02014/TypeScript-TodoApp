54aef4c997319a71f5b3c75d9ac7cc86779fba3d
JeonMinJae <ssi02014@naver.com>
2021-06-17 22:29:54 +0900

    Add(minjae/generic): TodoCollection Map 도입 #4

--- src/TodoCollection.ts
+++ src/TodoCollection.ts
@@ -2,20 +2,40 @@ import TodoItem from "./TodoItem";
 
 class TodoCollection {
   private nextId: number = 1;
+  private itemMap: Map<number, TodoItem>; //<key, value>
 
-  constructor(public userName: string, public todoItems: TodoItem[] = []) {}
+  constructor(public userName: string, todoItems: TodoItem[] = []) {
+    this.itemMap = new Map<number, TodoItem>();
+    todoItems.forEach((item) => this.itemMap.set(item.id, item));
+  }
 
   getTodoById(id: number): TodoItem | undefined {
-    return this.todoItems.find((item) => item.id === id);
+    return this.itemMap.get(id);
   }
 
   addTodo(task: string): number {
     while (this.getTodoById(this.nextId)) {
       this.nextId++;
     }
-    this.todoItems.push(new TodoItem(this.nextId, task));
+    this.itemMap.set(this.nextId, new TodoItem(this.nextId, task));
 
     return this.nextId;
+  }
+
+  //includeComplete - true : 모든 할일 목록
+  //includeComplete - false : 완료 목록은 제외한 할일 목록
+  getTodoItems(incluteComplete: boolean):TodoItem[] {
+    return [...this.itemMap.values()].filter(
+      (item) => incluteComplete || !item.complete
+    );
+  }
+
+  removeComplete(): void {
+    this.itemMap.forEach((item) => {
+      if(item.complete) {
+        this.itemMap.delete(item.id);
+      }
+    });
   }
 
   markComplete(id: number, complete: boolean): void {
